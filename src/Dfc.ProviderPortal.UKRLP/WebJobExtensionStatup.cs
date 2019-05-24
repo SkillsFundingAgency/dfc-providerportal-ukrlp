d0a0561292598b7c102f8bd387b4027e42771cb5
Mahmood Akhtar <mahmood233@hotmail.com>
2019-05-24 16:00:49 +0100

    COUR-779 Adding swagger for APIM and removing not needed project from solution. No changes to endpoints or how they work.

---
+++ src/Dfc.ProviderPortal.UKRLP/WebJobExtensionStatup.cs
@@ -0,0 +1,24 @@
+﻿using Dfc.ProviderPortal.UKRLP;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: WebJobsStartup(typeof(WebJobExtensionStatup), "Web Jobs Extension Startup")]
+
+namespace Dfc.ProviderPortal.UKRLP
+{
+    public class WebJobExtensionStatup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .Build()
+                .StartAsync();
+        }
+    }
+}

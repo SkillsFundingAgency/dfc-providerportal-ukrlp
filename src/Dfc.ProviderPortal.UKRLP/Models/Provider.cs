12fad1b27df82ca1e4f47fd7a64a770b268db4d0
Joe Hudson-Small <Joehs@me.com>
2023-03-21 15:33:30 +0000

    Adds NCSLT-3658 to Release 34 (#71)
    * Updated Provider SearchIndex to use en.microsoft analyzer for name field
    
    * Remove Both member from ProviderType (#61)
    
    * Fix DateUpdated type to be Nullable<DateTime>
    
    * consolidated ai to CD
    
    * NCSD-5012-upgrade-function-to-v3
    
    * Upgrade Repo to .net 6.0
    
    * Update FUNCTIONS_EXTENSION_VERSION to 4
    
    ---------
    
    Co-authored-by: James Law <james.law@alterius.co.uk>
    Co-authored-by: jameslawesynergy <68222608+jameslawesynergy@users.noreply.github.com>
    Co-authored-by: James Gunn <james@gunn.io>
    Co-authored-by: mofais <raymar786@gmail.com>
    Co-authored-by: mofais <51725650+mofais@users.noreply.github.com>
    Co-authored-by: olusola-adio <olusola_adio@hotmail.com>
    Co-authored-by: OLUSOLA ADIO <61968373+olusola-adio@users.noreply.github.com>
    Co-authored-by: amjones99 <59894100+amjones99@users.noreply.github.com>

--- src/Dfc.ProviderPortal.UKRLP/Models/Provider.cs
+++ src/Dfc.ProviderPortal.UKRLP/Models/Provider.cs
@@ -30,7 +30,7 @@ namespace Dfc.ProviderPortal.Providers
         public IVerificationdetail[] VerificationDetails { get; set; }
         public Status Status { get; set; }
         public DateTime DateDownloaded { get; set; }
-        public DateTime DateOnboarded { get; set; }
+        public DateTime? DateOnboarded { get; set; }
         public DateTime DateUpdated { get; set; }
         public string UpdatedBy { get; set; }
 
@@ -130,7 +130,6 @@ namespace Dfc.ProviderPortal.Providers
     {
          undefined = 0,
          Fe = 1,
-         Apprenticeship = 2,
-         Both = Fe | Apprenticeship
+         Apprenticeship = 2
     }
 }
